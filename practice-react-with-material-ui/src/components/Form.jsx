72b928b4d305098a1f7dd5b973d124a7189df675
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-05-29 20:53:56 +0500

    Form is completed

--- practice-react-with-material-ui/src/components/Form.jsx
+++ practice-react-with-material-ui/src/components/Form.jsx
@@ -1,11 +1,14 @@
 import { React, useState } from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, FormControl, InputLabel,Select,MenuItem, Checkbox, FormControlLabel, FormGroup, FormLabel, RadioGroup,Radio } from "@mui/material";
 
 function Form() {
   const [inputs, setInputs] = useState({
     name: "",
     email: "",
     password: "",
+    courses:"",
+    terms:true,
+    gender:""
   });
 
   const handleChange = (e) => {
@@ -53,6 +56,35 @@ function Form() {
           sx={{ margin: "20px" }}
         ></TextField>
         <br />
+        <FormGroup>
+          <FormControlLabel label="I Agree T&C" 
+          control={<Checkbox defaultChecked 
+          onChange={()=>
+          setInputs((prevState)=>
+          ({...prevState,terms:!inputs.terms}))}
+          /> }></FormControlLabel>
+        </FormGroup>
+        <br />
+        <FormControl>
+          <FormLabel>Gender</FormLabel>
+          <RadioGroup name="gender" defaultValue={'male'} onChange={handleChange}>
+          <FormControlLabel  value={'male'} label="Male" control={<Radio />}></FormControlLabel>
+          <FormControlLabel  value={'female'} label="Femalle" control={<Radio />}></FormControlLabel>
+          <FormControlLabel  value={'other'} label="Others" control={<Radio />}></FormControlLabel>
+          </RadioGroup>
+        </FormControl>
+        <br />
+
+         <FormControl fullWidth>
+          <InputLabel id="menu">Courses</InputLabel>
+          <Select name="courses" label="courses" value={inputs.courses} onChange={handleChange}>
+           <MenuItem value={"mongodb"}>Mongodb</MenuItem>
+           <MenuItem value={"react"}>React</MenuItem>
+           <MenuItem value={"node"}>Node</MenuItem>
+           <MenuItem value={"express"}>Express</MenuItem>
+          </Select>
+         </FormControl>
+
         <Button type="submit" variant="outlined">
           Submit
         </Button>
