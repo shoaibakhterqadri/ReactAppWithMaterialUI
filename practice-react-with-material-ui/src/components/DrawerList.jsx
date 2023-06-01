2ac7497056cd4e76bc4616ac1952427a9b4aac40
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-06-01 21:01:33 +0500

    Snackbar, CircularProgress, LinearProgress,  and Rating are done

--- practice-react-with-material-ui/src/components/DrawerList.jsx
+++ practice-react-with-material-ui/src/components/DrawerList.jsx
@@ -1,9 +1,10 @@
 import { Button } from '@mui/base';
-import { Drawer, List, ListItemButton, ListItemText } from '@mui/material';
+import { Alert, CircularProgress, Drawer, LinearProgress, List, ListItemButton, ListItemText, Rating, Snackbar, Typography } from '@mui/material';
 import React, { useState } from 'react'
 
 function DrawerList() {
     const [open,setOpen]=useState(false);
+    const [value,setValue]=useState()
     const courses=['first','second','third','fourth']
   return (
     <>
@@ -15,6 +16,14 @@ function DrawerList() {
             })}
         </List>
     </Drawer>
+    
+    {/* <Snackbar open={open} message="This is Snackbar"></Snackbar> */}
+    <Snackbar open={open} autoHideDuration={2000}><Alert severity='primary'>Alert is a built function</Alert></Snackbar>
+
+<CircularProgress color='primary' variant='determinate' value={50}></CircularProgress>
+<LinearProgress></LinearProgress>
+<Rating size='large' value={value} precision={0.2} onChange={(e,val)=>setValue(val)}></Rating>
+<Typography>Stars Rated is {value!==undefined ?value:0}</Typography>
     </>
   )
 }
